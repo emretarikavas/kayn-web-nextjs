58d14f5ac47afc637b5f09812348b2f59e1ac6da
tarikkavas <tarik.kavas@hotmail.com>
2021-10-31 01:12:20 +0300

    initial

---
+++ components/icons/index.js
@@ -0,0 +1,8 @@
+export { default as Cockroach } from './Cockroach'
+export { default as Digging } from './Digging'
+export { default as Fertilizer } from './Fertilizer'
+export { default as Group331 } from './Group331'
+export { default as Leaf } from './Leaf'
+export { default as Pesticide } from './Pesticide'
+export { default as TractorSideView } from './TractorSideView'
+export { default as Watering } from './Watering'
